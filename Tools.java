4a712b4620651106c7487d182b8b54c208715e4c
xivv <saverio_mancuso@gmx.de>
2019-10-09 09:30:35 +0200

    Update Tools.java

--- Tools.java
+++ Tools.java
@@ -8,15 +8,39 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+import org.apache.commons.io.FilenameUtils;
+
 public class Tools {
 
+	/**
+	 * Konfiguration
+	 */
 	private static final String CHARACTERURL = "https://na.leagueoflegends.com/en/game-info/champions/";
+
+	private static final String[] IMAGEFORMATS = { "png" };
 
 	private Tools() {
 
 	}
 
 	public static void grabCharacterImages() {
+
+	}
+
+	public static void resizeImages(String path, Integer size) {
+
+		File folder = new File(path);
+
+		for (File file : folder.listFiles())
+			if (!file.isDirectory() && FilenameUtils.isExtension(file.getName(), IMAGEFORMATS)) {
+
+				BufferedImage image = loadBufferedImage(file.getAbsolutePath());
+
+				if (image.getHeight() != size) {
+					BufferedImage resizedImage = resize(image, size, size);
+					saveBufferedImage(resizedImage, file.getAbsolutePath());
+				}
+			}
 
 	}
 
@@ -45,6 +69,26 @@ public class Tools {
 		// Return the buffered image
 		return bimage;
 
+	}
+
+	private static BufferedImage resize(BufferedImage img, int height, int width) {
+		Image tmp = img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+		BufferedImage resized = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		Graphics2D g2d = resized.createGraphics();
+		g2d.drawImage(tmp, 0, 0, null);
+		g2d.dispose();
+		return resized;
+	}
+
+	public static BufferedImage loadBufferedImage(String path) {
+
+		try {
+			File bufferedImage = new File(path);
+			return ImageIO.read(bufferedImage);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 	public static void saveBufferedImage(BufferedImage image, String path) {
